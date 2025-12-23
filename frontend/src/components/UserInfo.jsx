050fbfb700caafdf827755327750e9d2be42f690
deepss96 <111363738+deepss96@users.noreply.github.com>
2025-12-23 11:47:59 +0530

    Show actual password for temp users (is_temp_password=1) and mask with ***** for regular users in user management

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -174,7 +174,7 @@ const UserInfo = ({ selectedUser, onClose }) => {
                                 />
                                 <DisplayField
                                     label="PASSWORD"
-                                    value="*****"
+                                    value={selectedUser.is_temp_password === 1 ? selectedUser.password : "*****"}
                                     icon={FiLock}
                                 />
                                 <DisplayField
